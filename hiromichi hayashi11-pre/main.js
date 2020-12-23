16fab49f84aad696b0376a752f54c344aea50cfe
hiromichi-hayashi <75676003+hiromichi-hayashi@users.noreply.github.com>
2020-12-23 22:57:43 +0900

    訂正箇所の訂正完了
    高さなどの訂正完了
    スムーススクロールの実装

--- hiromichi hayashi11-pre/main.js
+++ hiromichi hayashi11-pre/main.js
@@ -1,5 +1,20 @@
 $(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
     $('#hammenu').on('click', function () {
-        $('.navmenu').toggleClass('open')
+        $('.navmenu,#hammenu,.first-view-catch__sp').toggleClass('open')
     })
+    $('.navmenu').on('click', function () {
+        $('.navmenu,#hammenu,.first-view-catch__sp').toggleClass('open')
+    })
+    $('.navmenu a[href]').on('click', function () {
+        $('#hammenu').trigger('click')
+    })
+
 })
